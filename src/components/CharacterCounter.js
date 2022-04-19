445e756b9f4d3d82f7b03d3f59800e219373dc11
Emi <emi_chang@lhwlfw.com>
2022-04-19 15:33:09 +0800

    add selector

--- src/components/CharacterCounter.js
+++ src/components/CharacterCounter.js
@@ -1,12 +1,15 @@
 import {
     atom,
     useRecoilState,
+    selector,
+    useRecoilValue,
 } from 'recoil';
 
 const textState = atom({
     key: 'textState', // unique ID (with respect to other atoms/selectors)
     default: 'Initial String', // default value (aka initial value)
 });
+
 
 export default function CharacterCounter() {
     const [text, setText] = useRecoilState(textState);
@@ -19,7 +22,22 @@ export default function CharacterCounter() {
         <div>
             <input type="text" value={text} onChange={onChange} />
             <br />
-            Echo: {text}
+            Echo: {text}          
+            <CharacterCount/>  
         </div>
     );
+}
+
+const charCountState = selector({
+    key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+    get: ({ get }) => {
+        const text = get(textState);
+        return text.length;
+    },
+});
+
+function CharacterCount() {
+    const count = useRecoilValue(charCountState);
+
+    return <>Character Count: {count}</>;
 }
